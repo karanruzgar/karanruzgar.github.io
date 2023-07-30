d20d5b27763e9f2c0ff87124c2af7f04bafd1b79
Karan Rüzgar Aluç <58516560+karanruzgar@users.noreply.github.com>
2023-07-30 09:58:24 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 en=["1","2","3","4","5","6","7","8"]
-passant=["f6","c6","h6","e6","e6","h6","c6","b6"]
+passant=["exf6","dxc6","gxh6","dxe6","dxe6","gxh6","dxc6","cxb6"]
+insults1 = [
+    "Do you think you play well?",
+    "Stop watching levy.",
+    "The only thing you can make is finding en passants.",
+    "You call that en passant? More like a en peasant!",
+    "wtf was that en passant",
+    "That was the worst en passant I've ever seen"
+]
+insults2 = [
+    "This guy can't even find an en passant.",
+    "You call that a move?",
+    "You don't know what en passant is?",
+    "You are spending so much time on r/AnarchChess",
+    "Play some chess!"
+]
 epc = 0
 
 pos = document.getElementById("pos")
@@ -17,13 +32,15 @@ function control(){
     answer = document.getElementById("answer")
     state = document.getElementById("state")
     console.log(ans)
-    if (answer.value.toLowerCase() == ans){
+    if (answer.value.toLowerCase() == ans || answer.value.toLowerCase() == ans.split("x")[1]){
         state.innerHTML = "True"
         state.style.color = "green"
+        document.getElementById("ai-text").innerHTML = insults1[(Math.floor(Math.random() * insults1.length))]
     }
     else{
         state.style.color = "red"
         state.innerHTML = "False. <a href='https://www.google.com/search?q=en+passant' target=”_blank”>Google en passant</a>"
+        document.getElementById("ai-text").innerHTML = insults2[(Math.floor(Math.random() * insults2.length))]
     }
     answer.value = ""
     change()
